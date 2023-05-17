117c649e73f9199890a9a171efa649597114b0a9
giangcm <111730947+giangcm@users.noreply.github.com>
2023-05-17 22:16:12 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import HomePage from "./pages/HomePage";
+
+import "./App.scss";
+import GameShopPage from "./pages/GameShopPage/GameShopPage";
+import MyGamesPage from "./pages/MyGamesPage/MyGamesPage";
+import MyFavoritesPage from "./pages/MyFavoritesPage/MyFavoritesPage";
+import GamePage from "./pages/GamePage/GamePage";
 
 function App() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const day = String(today.getDate()).padStart(2, "0");
+  const formattedDate = `${year}-${month}-${day}`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app w-90">
+        <NavBar />
+
+        <Routes>
+          <Route path="*" element={<HomePage />} />
+          <Route
+            path="/gameshop"
+            element={<GameShopPage today={formattedDate} />}
+          />
+          <Route
+            path="/mygames"
+            element={<MyGamesPage today={formattedDate} />}
+          />
+          <Route path="/favorites" element={<MyFavoritesPage />} />
+          <Route path="/game/:gameId" element={<GamePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
